14e185b56b395709a258d0a82fa0ec775fec9806
KirkBuah <riccardobuah@gmail.com>
2024-12-24 09:57:53 +0100

    add color attribute to node

--- node.js
+++ node.js
@@ -1,11 +1,17 @@
 class Node {
     constructor(value) {
         this.value = value;
+        this.color = color(0);
         this.x = random(width);
         this.y = random(height);
     }
 
     static nodeEquals(node1, node2) {
         return node1.value === node2.value;
+    }
+
+    draw() {
+        fill(this.color);
+        circle(this.x, this.y, 10);
     }
 }
